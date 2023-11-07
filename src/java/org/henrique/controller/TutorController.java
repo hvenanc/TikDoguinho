f30fbdc58f24f7a7265e876a594af1fb8aa03d92
hvenanc <hvs@poli.br>
2023-11-06 21:06:02 -0300

    Implementação da alteração de senha do Tutor. Implementações Básicas referente ao Tutor Concluidas

--- src/java/org/henrique/controller/TutorController.java
+++ src/java/org/henrique/controller/TutorController.java
@@ -9,6 +9,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 import org.henrique.model.dao.ManagerDao;
 import org.henrique.model.negocios.Tutor;
 
@@ -39,6 +40,29 @@ public class TutorController {
     public void alterar() {
         ManagerDao.getCurrentInstance().update(this.selection);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Dados atualizados com sucesso"));
+    }
+    
+    
+    public void alterarSenha(String senha, String novaSenha, String confimacao) {
+        
+        Tutor tutorLogado = ((LoginController) ((HttpSession) FacesContext.getCurrentInstance()
+                .getExternalContext().getSession(true))
+                .getAttribute("loginController")).getTutorLogado();
+        
+        if(!tutorLogado.getSenha().equals(senha)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Senha Incorreta!"));
+            return;
+        }
+        
+        if(!novaSenha.equals(confimacao)) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("A confirmação não bate com a nova senha!"));
+            return;
+        }
+        
+        tutorLogado.setSenha(novaSenha);
+        ManagerDao.getCurrentInstance().update(tutorLogado);
+         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Senha Alterada com Sucesso!"));
+        
     }
 
     public Tutor getTutorCadastro() {
